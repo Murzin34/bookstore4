43510e146230ee1e1989e51159ae8f57de901386
Murzin34 <108129795+Murzin34@users.noreply.github.com>
2024-08-02 11:03:06 +0300

    step 2 from BJS2-17615

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("The store is expected to open in 4 days");
     }
 }
